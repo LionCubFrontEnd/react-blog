49202c81a4dfa82b8020c012d0a2521103287d5e
Chocolate1999 <2816778271@qq.com>
2022-01-15 20:59:32 +0800

    feat: 更新 url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: '前端',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://yangchaoyi.vip/react',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
